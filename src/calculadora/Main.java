e38053a42f6d0e83164601f57a220b5e1d25887b
gabriellaraujo20 <48826478+gabriellaraujo20@users.noreply.github.com>
2019-04-05 19:42:50 -0300

    commit inicial
    Primeiro commit

---
+++ src/calculadora/Main.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+import calculadora.views.TelaPrincipal;
+
+/* @author gabri*/
+public class Main {
+
+    public static void main(String[] args) {
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        
+        telaPrincipal.setVisible(true);
+        
+    }
+
+}
