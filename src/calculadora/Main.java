3c53ce3ad9a99d681ad480125329555778f9eb9e
gabriellaraujo20 <48826478+gabriellaraujo20@users.noreply.github.com>
2019-04-09 21:30:31 -0300

    Implementação Raiz Quadrada
    Finalizei a Classe Rais Quadrada

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -1,14 +1,16 @@
 package calculadora;
 
+import Calculo.RaizQuadrada;
 import calculadora.views.FrmCalculadoraController;
 
-
-/* @author gabri*/
+/* @author gabriel*/
 public class Main {
 
     public static void main(String[] args) {
         FrmCalculadoraController telaCalc = new FrmCalculadoraController();
+        RaizQuadrada raizQuadrada = new RaizQuadrada();
         
+        raizQuadrada.raizQuadrada(49);
         telaCalc.show(args);
         
     }
