b72b8de07cf9e6e54d61a04747e38412f6100f81
Bruno Mattos <Bruno.mattos142@gmail.com>
2019-04-09 21:56:30 -0300

    Divisao
    divisao feita

---
+++ src/Calculo/Divisao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Divisao {
+   
+ 
+
+   public double divisao(double v1 , double v2){
+   return v1/v2;   
+   }
+}
---
+++ src/Calculo/teste/TesteDivisao.java
@@ -0,0 +1,21 @@
+
+package Calculo.teste;
+import Calculo.Divisao;
+
+/**
+ *
+ * @author Bruno
+ */
+public class TesteDivisao {
+    
+    public static void main(String [] args){
+    
+     Divisao divisao = new Divisao();
+     
+        System.out.println(divisao.divisao(10, 2));
+        
+        
+    }
+        
+       
+}
